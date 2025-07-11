701cb584b07a56a2f76b573207bb281de3f81489
Jackson Walters <jacksonwalters@gmail.com>
2025-07-11 16:01:35 -0400

    update

--- pushout_boolean_algebras/Utils/Basic.lean
+++ pushout_boolean_algebras/Utils/Basic.lean
@@ -1,5 +1,5 @@
-import Mathlib.Topology.Category.TopCat.Basic
-import Mathlib.Topology.Category.TopCat.Limits.Pullbacks
-import Mathlib.Topology.Category.Profinite.Basic
+-- import Mathlib.Topology.Category.TopCat.Basic
+-- import Mathlib.Topology.Category.TopCat.Limits.Pullbacks
+-- import Mathlib.Topology.Category.Profinite.Basic
 
 def hello := "world"
