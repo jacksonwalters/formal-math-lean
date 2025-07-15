a85b2e4c27b543157cdbdf67b78253b5602900ee
Jackson Walters <jacksonwalters@gmail.com>
2025-07-15 17:37:14 -0400

    add set_option compiler.extract

--- pushout_boolean_algebras/Main.lean
+++ pushout_boolean_algebras/Main.lean
@@ -1,4 +1,6 @@
 import Utils.Basic
 
+set_option compiler.extract_closed false
+
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
--- pushout_boolean_algebras/Utils/Basic.lean
+++ pushout_boolean_algebras/Utils/Basic.lean
@@ -1,4 +1,4 @@
--- import Mathlib.Topology.Category.TopCat.Basic
+import Mathlib.Topology.Category.TopCat.Basic
 -- import Mathlib.Topology.Category.TopCat.Limits.Pullbacks
 -- import Mathlib.Topology.Category.Profinite.Basic
 
